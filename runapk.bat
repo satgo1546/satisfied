0935e22716c6c6b240af0e9802b6371d027d144d
Frog Chen <satgo1546@yeah.net>
2021-09-09 11:39:19 +0800

    Refresh slzapk.c
    My coding & naming style has changed over time. Global variables are quick and
    dirty and I love them so much, but to accommodate emission of multiple files
    in one program, packing them into structures is still the best way, which is
    so frustrating, so I choose to abuse the C++ keyword “this”.

--- runapk.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-gcc -o slzapk.exe slzapk.c
-if errorlevel 1 goto :eof
-slzapk.exe
-if errorlevel 1 goto :eof
-Q:\Prgm\WinRAR\WinRAR.exe slzapk-output.apk
--- slzapk.c
+++ slzapk.c
@@ -14,182 +14,168 @@
    crc_reflected() in https://www.zlib.net/crc_v3.txt
    or Garry S. Brown's implementation e.g. in
    https://opensource.apple.com/source/xnu/xnu-792.13.8/bsd/libkern/crc32.c */
-uint32_t crc32(FILE *f, size_t n) {
+uint32_t crc32(FILE *fp) {
 	static uint32_t crc32_table[256];
-	if (!crc32_table[0]) {
+	if (!*crc32_table) {
 		/* Based on Figure 14-7 in Hacker's Delight (2nd ed.) Equivalent to
 				cm_tab() in https://www.zlib.net/crc_v3.txt with
 				cm_width = 32, cm_refin = true, cm_poly = 0xcbf43926. */
-		for (uint32_t idx = 0; idx < 256; idx++) {
-			uint32_t crc = idx;
-			uint32_t mask;
-			for (int i = 0; i < 8; i++) {
-				mask = -(crc & 1);
-				crc = (crc >> 1) ^ (0xedb88320 & mask);
+		for (uint32_t i = 0; i < 256; i++) {
+			uint32_t x = i;
+			for (int j = 0; j < 8; j++) {
+				x = (x >> 1) ^ (0xedb88320 & -(x & 1));
 			}
-			crc32_table[idx] = crc;
+			crc32_table[i] = x;
 		}
 	}
-	uint32_t crc = 0xffffffff;
-	while (n--) {
-		crc = (crc >> 8) ^ crc32_table[(crc ^ fgetc(f)) & 0xff];
+	uint32_t r = 0xffffffff;
+	for (;;) {
+		int ch = fgetc(fp);
+		if (ch == EOF) break;
+		r = (r >> 8) ^ crc32_table[(r ^ ch) & 0xff];
 	}
-	return ~crc;
+	return ~r;
 }
 
-struct {
-	size_t lfh_begin;
-	size_t contents_begin;
-	size_t end;
-	bool binary;
-	uint32_t external_file_attributes;
-} zip_files[100];
-size_t zip_file_count;
-bool zip_file_began;
+struct zip_archive {
+	FILE *fp;
+	struct {
+		long lfh_start;
+		long contents_start;
+		bool binary;
+		uint32_t external_file_attributes;
+	} files[100];
+	size_t file_count;
+	bool file_began;
+};
 
-void zip_begin_file(FILE *f, const char *filename, const char *mode, size_t contents_alignment) {
-	assert(!zip_file_began);
-	zip_file_began = true;
+void zip_begin_archive_fp(struct zip_archive *this, FILE *fp) {
+	this->fp = fp;
+	this->file_count = 0;
+	this->file_began = false;
+}
+
+void zip_begin_archive(struct zip_archive *this, const char *filename) {
+	FILE *fp = fopen(filename, "wb+");
+	if (!fp) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+	zip_begin_archive_fp(this, fp);
+}
+
+void zip_begin_file(struct zip_archive *this, const char *filename, const char *mode, size_t contents_alignment) {
+	assert(!this->file_began);
 	assert(mode);
+	assert(contents_alignment);
+	assert(filename && *filename);
+	if (this->file_count >= 100) {
+		fprintf(stderr, "%s: too many files\n", __func__);
+		exit(EXIT_FAILURE);
+	}
+
+	this->file_began = true;
+	this->files[this->file_count].binary = false;
+	this->files[this->file_count].external_file_attributes = 0;
 	while (*mode) switch (*mode++) {
 	case 'b':
-		zip_files[zip_file_count].binary = true;
+		this->files[this->file_count].binary = true;
 		break;
 	}
 
-	size_t lfh_begin = ftell(f);
-	size_t contents_begin = lfh_begin + 30 + strlen(filename);
-	assert(contents_alignment);
-	size_t padding = align_to(contents_begin, contents_alignment) - contents_begin;
+	long lfh_start = ftell(this->fp);
+	long contents_start = lfh_start + 30 + strlen(filename);
+	long padding = align_to(contents_start, contents_alignment) - contents_start;
 	// The padding must be in the extra fields, not before local file headers, or Android stops parsing it despite it being valid ZIP and JAR.
-	contents_begin += padding;
-	zip_files[zip_file_count].lfh_begin = lfh_begin;
-	zip_files[zip_file_count].contents_begin = contents_begin;
-	write8(f, 'P');
-	write8(f, 'K');
-	write8(f, 3);
-	write8(f, 4);
+	contents_start += padding;
+	this->files[this->file_count].lfh_start = lfh_start;
+	this->files[this->file_count].contents_start = contents_start;
 	bool folder = strchr("/\\", filename[strlen(filename) - 1]);
-	if (folder) zip_files[zip_file_count].external_file_attributes |= 1 << 5;
-	write16(f, folder ? 20 : 10); // version needed to extract = PKZip 2.0
-	write16(f, 0); // general purpose bit flag = not encrypted
-	write16(f, 0); // compression method = stored
+	if (folder) this->files[this->file_count].external_file_attributes |= 1 << 5;
+
+	fputs("PK\3\4", this->fp);
+	write16(this->fp, folder ? 20 : 10); // version needed to extract = PKZip 2.0
+	write16(this->fp, 0); // general purpose bit flag = not encrypted
+	write16(this->fp, 0); // compression method = stored
 	{
 		time_t t = time(NULL);
 		struct tm *tm = localtime(&t);
-		write16(f, (tm->tm_sec / 2) | (tm->tm_min << 5) | (tm->tm_hour << 11));
+		write16(this->fp, (tm->tm_sec / 2) | (tm->tm_min << 5) | (tm->tm_hour << 11)); // last mod file time
 		// Bits 0--4:  Second divided by two.  Bits 5--10: Minute.  Bits 11-15: Hour.
-		write16(f, tm->tm_mday | ((tm->tm_mon + 1) << 5) | ((tm->tm_year - 80) << 9));
+		write16(this->fp, tm->tm_mday | ((tm->tm_mon + 1) << 5) | ((tm->tm_year - 80) << 9)); // last mod file date
 		// Bits 0--4:  Day (1--31).  Bits 5--8:  Month (1--12).  Bits 9--15: Year from 1980.
 	}
-	write32(f, 0); // CRC32
-	write32(f, 0); // compressed size
-	write32(f, 0); // uncompressed size
-	assert(filename && *filename);
-	write16(f, strlen(filename)); // filename length
-	write16(f, padding); // extra field length
-	fputs(filename, f); // filename
-	while (padding--) fputc(0, f); // extra field
-	zip_file_count++;
+	write32(this->fp, 0); // CRC32 (to be calculated)
+	write32(this->fp, 0); // compressed size (to be calculated)
+	write32(this->fp, 0); // uncompressed size (to be calculated)
+	write16(this->fp, strlen(filename)); // filename length
+	write16(this->fp, padding); // extra field length
+	fputs(filename, this->fp); // filename
+	while (padding--) fputc(0, this->fp); // extra field
+	this->file_count++;
 }
 
-void zip_end_file(FILE *f) {
-	assert(zip_file_began);
-	zip_file_began = false;
-	zip_files[zip_file_count - 1].end = ftell(f);
-	seek(f, zip_files[zip_file_count - 1].contents_begin);
-	size_t size = zip_files[zip_file_count - 1].end - zip_files[zip_file_count - 1].contents_begin;
-	uint32_t crc = crc32(f, size);
-	seek(f, zip_files[zip_file_count - 1].lfh_begin + 14);
-	write32(f, crc); // CRC32
-	write32(f, size); // compressed size
-	write32(f, size); // uncompressed size
-	seek(f, zip_files[zip_file_count - 1].end);
+void zip_end_file(struct zip_archive *this) {
+	assert(this->file_began);
+	this->file_began = false;
+	long size = ftell(this->fp) - this->files[this->file_count - 1].contents_start;
+	fseek(this->fp, this->files[this->file_count - 1].contents_start, SEEK_SET);
+	uint32_t crc = crc32(this->fp);
+	fseek(this->fp, this->files[this->file_count - 1].lfh_start + 14, SEEK_SET);
+	write32(this->fp, crc); // CRC32
+	write32(this->fp, size); // compressed size
+	write32(this->fp, size); // uncompressed size
+	fseek(this->fp, 0, SEEK_END);
 }
 
-void zip_write_cd(FILE *f) {
+void zip_end_archive(struct zip_archive *this) {
 	static unsigned char buffer[600];
-	assert(!zip_file_began);
-	size_t cd_begin = ftell(f);
-	for (size_t i = 0; i < zip_file_count; i++) {
-		size_t lfh_size = zip_files[i].contents_begin - zip_files[i].lfh_begin;
-		seek(f, zip_files[i].lfh_begin);
-		fread(buffer, 1, lfh_size, f);
-		fseek(f, 0, SEEK_END);
-		write8(f, 'P');
-		write8(f, 'K');
-		write8(f, 1);
-		write8(f, 2);
-		write16(f, (0 << 8) | 0); // version made by = MS-DOS 0.0
-		fwrite(buffer + 4, 1, 26, f);
-		write16(f, 0); // file comment length
-		write16(f, 0); // disk number start
-		write16(f, !zip_files[i].binary); // internal file attributes
-		write32(f, zip_files[i].external_file_attributes); // external file attributes
-		write32(f, zip_files[i].lfh_begin); // relative offset of local header
-		fwrite(buffer + 30, 1, lfh_size - 30, f);
-		(void) f; // file comment
+	assert(!this->file_began);
+	long cd_start = ftell(this->fp);
+	for (size_t i = 0; i < this->file_count; i++) {
+		long lfh_size = this->files[i].contents_start - this->files[i].lfh_start;
+		fseek(this->fp, this->files[i].lfh_start, SEEK_SET);
+		fread(buffer, 1, lfh_size, this->fp);
+		fseek(this->fp, 0, SEEK_END);
+		fputs("PK\1\2", this->fp);
+		write16(this->fp, (0 << 8) | 0); // version made by = MS-DOS 0.0
+		fwrite(buffer + 4, 1, 26, this->fp);
+		write16(this->fp, 0); // file comment length
+		write16(this->fp, 0); // disk number start
+		write16(this->fp, !this->files[i].binary); // internal file attributes
+		write32(this->fp, this->files[i].external_file_attributes); // external file attributes
+		write32(this->fp, this->files[i].lfh_start); // relative offset of local header
+		fwrite(buffer + 30, 1, lfh_size - 30, this->fp);
+		(void) this->fp; // file comment
 	}
-	size_t cd_end = ftell(f);
-	write8(f, 'P');
-	write8(f, 'K');
-	write8(f, 5);
-	write8(f, 6);
-	write16(f, 0); // number of this disk
-	write16(f, 0); // number of the disk with the start of the central directory
-	write16(f, zip_file_count); // total number of entries in the central dir on this disk
-	write16(f, zip_file_count); // total number of entries in the central dir
-	write32(f, cd_end - cd_begin); // size of the central directory
-	write32(f, cd_begin); // offset of start of central directory with respect to the starting disk number
-	write16(f, 0); // zipfile comment length
-	(void) f; // zipfile comment
+	long cd_size = ftell(this->fp) - cd_start;
+	fputs("PK\5\6", this->fp);
+	write16(this->fp, 0); // number of this disk
+	write16(this->fp, 0); // number of the disk with the start of the central directory
+	write16(this->fp, this->file_count); // total number of entries in the central dir on this disk
+	write16(this->fp, this->file_count); // total number of entries in the central dir
+	write32(this->fp, cd_size); // size of the central directory
+	write32(this->fp, cd_start); // offset of start of central directory with respect to the starting disk number
+	write16(this->fp, 0); // zipfile comment length
+	(void) this->fp; // zipfile comment
+	if (fclose(this->fp) == EOF) {
+		perror("fclose");
+		exit(EXIT_FAILURE);
+	}
 }
 
 
 int main(int argc, char **argv) {
-	FILE *f;
-	size_t i;
-	uint8_t* classes;
-	size_t classes_sz = read_file("runapk.bat", (void**) &classes);
+	struct zip_archive f;
+	zip_begin_archive(&f, "slzapk-output.apk");
 
-	f = fopen("slzapk-output.apk", "wb+");
-	if (!f) {
-		perror("fopen");
-		return 1;
-	}
+	zip_begin_file(&f, "AndroidManifest.xml", "b", 4);
+	zip_end_file(&f);
+	zip_begin_file(&f, "resources.arsc", "b", 4);
+	zip_end_file(&f);
+	zip_begin_file(&f, "classes.dex", "b", 4);
+	copy_file(f.fp, "bits.c");
+	zip_end_file(&f);
 
-	void zip_fff(const char *filename) {
-		char a[100];
-		if (filename[strlen(filename)-1]=='/'){
-			//zip_begin_file(f, filename,"b",1);
-			//zip_end_file(f);
-		}else{
-			strcpy(a, "sto/");
-			strcat(a, filename);
-			zip_begin_file(f, filename,"b",4);
-			copy_file(f, a);
-			zip_end_file(f);
-		}
-	}
-zip_fff("res/layout/main.xml");
-zip_fff("AndroidManifest.xml");
-zip_fff("resources.arsc");
-zip_fff("res/drawable-hdpi/ic_launcher.png");
-zip_fff("res/drawable-hdpi/logo00001.PNG");
-zip_fff("res/drawable-hdpi/logo002.PNG");
-zip_fff("res/drawable-hdpi/logo003.PNG");
-zip_fff("res/drawable-ldpi/ic_launcher.png");
-zip_fff("res/drawable-mdpi/ic_launcher.png");
-zip_fff("res/drawable-xhdpi/ic_launcher.png");
-zip_fff("classes.dex");
-zip_fff("META-INF/MANIFEST.MF");
-zip_fff("META-INF/CERT.SF");
-zip_fff("META-INF/CERT.RSA");
-zip_write_cd(f);
-
-	if (fclose(f) == EOF) {
-		perror("fclose");
-		return 1;
-	}
-	return 0;
+	zip_end_archive(&f);
 }
