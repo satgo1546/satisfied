90d23d32caa84e31a36a6afbbf637567da1eeea5
Frog Chen <satgo1546@yeah.net>
2021-09-15 08:28:51 +0800

    Add keystore.jks
    This is the private key used to sign small.apk.
    I void the “security” deliberately in case of problems.

--- adbenv.bat
+++ adbenv.bat
@@ -1,4 +1,4 @@
 @echo off
-path %PATH%;%ANDROID_SDK_ROOT%\tools;%ANDROID_SDK_ROOT%\platform-tools;%ANDROID_SDK_ROOT%\build-tools\25.0.3
+path %PATH%;%ANDROID_SDK_ROOT%\tools;%ANDROID_SDK_ROOT%\platform-tools;%ANDROID_SDK_ROOT%\build-tools\25.0.3;Q:\Prgm\Java\jdk1.8.0_131\bin
 cmd
 rem aapt d xmltree com.package.apk AndroidManifest.xml
--- jarsign.bat
+++ jarsign.bat
@@ -1,2 +1,4 @@
 keytool -genkeypair -keystore keystore.jks -alias a -validity 100000 -keyalg RSA -keysize 2048 -storepass 114514 -keypass 114514
-jarsigner -keystore keystore.jks -storepass 114514 -keypass 114514 -digestalg SHA-1 slzapk-output.apk a
+::jarsigner -verify -verbose slzapk-output.apk
+::apksigner verify --verbose slzapk-output.apk
+jarsigner -keystore keystore.jks -storepass 114514 -keypass 114514 -sigalg SHA1withRSA -digestalg SHA1 slzapk-output.apk a
