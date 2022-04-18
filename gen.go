24a05c24f45b00701f45c47541cf2eb8cfab0dba
Frog Chen <satgo1546@yeah.net>
2022-04-18 17:35:52 +0800

    Add NumberInstructions()

--- gen.go
+++ gen.go
@@ -52,6 +52,7 @@ type Subroutine struct {
 type Instruction struct {
 	Next   *Instruction
 	Info   int
+	Serial int
 	Opcode int
 	Arg0   *Instruction
 	Arg1   *Instruction
@@ -78,70 +79,80 @@ func Align(n int, m int) int {
 	return n - rem + m
 }
 
-var lastID int
-
-func (inst *Instruction) ID() int {
-	if inst.Info == 0 {
-		lastID++
-		inst.Info = lastID
+func NumberInstructions(inst *Instruction, start int) int {
+	for ; inst != nil; inst = inst.Next {
+		inst.Serial = start
+		start++
+		switch inst.Opcode {
+		case OpIfNonzero, OpIfPositive, OpIfNegative:
+			start = NumberInstructions(inst.Arg0, start)
+			start = NumberInstructions(inst.Arg1, start)
+			start = NumberInstructions(inst.Arg2, start)
+		case OpWhile:
+			start = NumberInstructions(inst.Arg0, start)
+		}
 	}
-	return inst.Info
+	return start
 }
 
 func emit_instructions(inst *Instruction, pred0 *Instruction) {
 	for ; inst != nil; inst = inst.Next {
-		emit_noindent(".L%d: ; %p = %+v", inst.ID(), inst, inst)
+		emit_noindent(".L%d: ; %p = %+v", inst.Serial, inst, inst)
 		switch inst.Opcode {
 		case OpΦ:
-			emit("mov eax, [esp-%d*4-4]", inst.Arg0.ID())
-			emit("test dword [esp-%d*4-4], 1", pred0.ID())
-			emit("cmovz eax, [esp-%d*4-4]", inst.Arg1.ID())
-			emit("mov [esp-%d*4-4], eax", inst.Info)
+			emit("mov eax, [esp+%d*4]", inst.Arg0.Serial)
+			emit("test dword [esp+%d*4], 1", pred0.Serial)
+			emit("cmovz eax, [esp+%d*4]", inst.Arg1.Serial)
+			emit("mov [esp+%d*4], eax", inst.Serial)
 		case OpReturn:
-			emit("mov eax, [esp-%d*4-4]", inst.Arg0.Info)
+			emit("mov eax, [esp+%d*4]", inst.Arg0.Serial)
+			emit("leave")
 			emit("ret")
 		case OpIfNonzero, OpIfPositive, OpIfNegative:
-			emit("cmp dword [esp-%d*4-4], 0", inst.Arg3.Info)
-			emit("%s .L%d_else", map[int]string{
+			emit("cmp dword [esp+%d*4], 0", inst.Arg3.Serial)
+			emit("%s .L%d", map[int]string{
 				OpIfNonzero:  "je",
 				OpIfPositive: "jle",
 				OpIfNegative: "jge",
-			}[inst.Opcode], inst.Info)
+			}[inst.Opcode], inst.Arg1.Serial)
 			emit_instructions(inst.Arg0, nil)
-			emit("mov dword [esp-%d*4-4], 1", inst.Info)
-			emit("jmp .L%d_end", inst.Info)
-			emit_noindent(".L%d_else:", inst.Info)
+			emit("mov dword [esp+%d*4], 1", inst.Serial)
+			emit("jmp .L%d", inst.Arg2.Serial)
 			emit_instructions(inst.Arg1, nil)
-			emit("mov dword [esp-%d*4-4], 0", inst.Info)
-			emit_noindent(".L%d_end:", inst.Info)
+			emit("mov dword [esp+%d*4], 0", inst.Serial)
 			emit_instructions(inst.Arg2, inst)
 		case OpWhile:
 			// TODO
 		case OpConst:
-			emit("mov dword [esp-%d*4-4], %d", inst.Info, inst.Const)
+			emit("mov dword [esp+%d*4], %d", inst.Serial, inst.Const)
 		case OpNot, OpNeg:
-			emit("mov eax, [esp-%d*4-4]", inst.Arg0.Info)
+			emit("mov eax, [esp+%d*4]", inst.Arg0.Serial)
 			emit("%s eax", map[int]string{
 				OpNot: "not",
 				OpNeg: "neg",
 			}[inst.Opcode])
-			emit("mov [esp-%d*4-4], eax", inst.Info)
+			emit("mov [esp+%d*4], eax", inst.Serial)
 		case OpAdd, OpSub, OpMul, OpAnd, OpOr, OpXor:
-			emit("mov eax, [esp-%d*4-4]", inst.Arg0.Info)
-			emit("%s eax, [esp-%d*4-4]", map[int]string{
+			emit("mov eax, [esp+%d*4]", inst.Arg0.Serial)
+			emit("%s eax, [esp+%d*4]", map[int]string{
 				OpAdd: "add",
 				OpSub: "sub",
 				OpMul: "imul",
 				OpAnd: "and",
 				OpOr:  "or",
 				OpXor: "xor",
-			}[inst.Opcode], inst.Arg1.Info)
-			emit("mov [esp-%d*4-4], eax", inst.Info)
+			}[inst.Opcode], inst.Arg1.Serial)
+			emit("mov [esp+%d*4], eax", inst.Serial)
 		}
 	}
 }
 
 func emit_subroutine(subroutine *Subroutine) {
 	emit_noindent("%s:", subroutine.Name)
+	// Reserve space appropriately first.
+	// https://devblogs.microsoft.com/oldnewthing/20190111-00/?p=100685
+	emit("push ebp")
+	emit("mov ebp, esp")
+	emit("sub esp, %d*4", NumberInstructions(subroutine.Code, 0))
 	emit_instructions(subroutine.Code, nil)
 }
